e5cafd47912b04749e715b511a30b17f2ef63c87
Jeremy Daly <jeremy@jeremydaly.com>
2018-03-15 12:31:46 -0400

    add tests for new location and redirect methods

--- test/responses.js
+++ test/responses.js
@@ -43,6 +43,26 @@ api.get('/testEmptyResponse', function(req,res) {
   res.send()
 })
 
+api.get('/location', function(req,res) {
+  res.location('http://www.github.com').html('Location header set')
+})
+
+api.get('/locationEncode', function(req,res) {
+  res.location('http://www.github.com?foo=bar with space').html('Location header set')
+})
+
+api.get('/redirect', function(req,res) {
+  res.redirect('http://www.github.com')
+})
+
+api.get('/redirect301', function(req,res) {
+  res.redirect(301,'http://www.github.com')
+})
+
+api.get('/redirectHTML', function(req,res) {
+  res.redirect('http://www.github.com?foo=bar&bat=baz<script>alert(\'not good\')</script>')
+})
+
 /******************************************************************************/
 /***  BEGIN TESTS                                                           ***/
 /******************************************************************************/
@@ -96,6 +116,56 @@ describe('Response Tests:', function() {
       api.run(_event,{},function(err,res) { resolve(res) })
     }).then((result) => {
       expect(result).to.deep.equal({ headers: { 'Content-Type': 'application/json' }, statusCode: 200, body: '' })
+    })
+  }) // end it
+
+  it('Location method', function() {
+    let _event = Object.assign({},event,{ path: '/location'})
+
+    return new Promise((resolve,reject) => {
+      api.run(_event,{},function(err,res) { resolve(res) })
+    }).then((result) => {
+      expect(result).to.deep.equal({ headers: { 'Content-Type': 'text/html', 'Location': 'http://www.github.com' }, statusCode: 200, body: 'Location header set' })
+    })
+  }) // end it
+
+  it('Location method (encode URL)', function() {
+    let _event = Object.assign({},event,{ path: '/locationEncode'})
+
+    return new Promise((resolve,reject) => {
+      api.run(_event,{},function(err,res) { resolve(res) })
+    }).then((result) => {
+      expect(result).to.deep.equal({ headers: { 'Content-Type': 'text/html', 'Location': 'http://www.github.com?foo=bar%20with%20space' }, statusCode: 200, body: 'Location header set' })
+    })
+  }) // end it
+
+  it('Redirect (default 302)', function() {
+    let _event = Object.assign({},event,{ path: '/redirect'})
+
+    return new Promise((resolve,reject) => {
+      api.run(_event,{},function(err,res) { resolve(res) })
+    }).then((result) => {
+      expect(result).to.deep.equal({ headers: { 'Content-Type': 'text/html', 'Location': 'http://www.github.com' }, statusCode: 302, body: '<p>302 Redirecting to <a href="http://www.github.com">http://www.github.com</a></p>' })
+    })
+  }) // end it
+
+  it('Redirect (301)', function() {
+    let _event = Object.assign({},event,{ path: '/redirect301'})
+
+    return new Promise((resolve,reject) => {
+      api.run(_event,{},function(err,res) { resolve(res) })
+    }).then((result) => {
+      expect(result).to.deep.equal({ headers: { 'Content-Type': 'text/html', 'Location': 'http://www.github.com' }, statusCode: 301, body: '<p>301 Redirecting to <a href="http://www.github.com">http://www.github.com</a></p>' })
+    })
+  }) // end it
+
+  it('Redirect (escape html)', function() {
+    let _event = Object.assign({},event,{ path: '/redirectHTML'})
+
+    return new Promise((resolve,reject) => {
+      api.run(_event,{},function(err,res) { resolve(res) })
+    }).then((result) => {
+      expect(result).to.deep.equal({ headers: { 'Content-Type': 'text/html', 'Location': 'http://www.github.com?foo=bar&bat=baz%3Cscript%3Ealert(\'not%20good\')%3C/script%3E' }, statusCode: 302, body: '<p>302 Redirecting to <a href=\"http://www.github.com?foo=bar&amp;bat=baz&lt;script&gt;alert(&#39;not good&#39;)&lt;/script&gt;\">http://www.github.com?foo=bar&amp;bat=baz&lt;script&gt;alert(&#39;not good&#39;)&lt;/script&gt;</a></p>' })
     })
   }) // end it
 
