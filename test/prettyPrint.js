b3b9e46a2a8f40f8c2d2f50d965f0825226ff10c
Jeremy Daly <jeremy@jeremydaly.com>
2018-05-29 22:37:58 -0400

    remove console.log from prettyPrint

--- test/prettyPrint.js
+++ test/prettyPrint.js
@@ -9,8 +9,12 @@ const prettyPrint = require('../lib/prettyPrint')
 /******************************************************************************/
 
 describe('PrettyPrint Tests:', function() {
-  it('Execute', function() {
-    expect(prettyPrint([['GET','/'],['POST','/'],['DELETE','/']])).to.equal(undefined)
+  it('Minimum header widths', function() {
+    expect(prettyPrint([['GET','/'],['POST','/'],['DELETE','/']])).to.equal('╔══════════╤═════════╗\n║  \u001b[1mMETHOD\u001b[0m  │  \u001b[1mROUTE\u001b[0m  ║\n╟──────────┼─────────╢\n║  GET     │  /      ║\n╟──────────┼─────────╢\n║  POST    │  /      ║\n╟──────────┼─────────╢\n║  DELETE  │  /      ║\n╚══════════╧═════════╝')
+  }) // end it
+
+  it('Adjusted header widths', function() {
+    expect(prettyPrint([['GET','/'],['POST','/testing'],['DELETE','/long-url-path-name']])).to.equal('╔══════════╤═══════════════════════╗\n║  \u001b[1mMETHOD\u001b[0m  │  \u001b[1mROUTE              \u001b[0m  ║\n╟──────────┼───────────────────────╢\n║  GET     │  /                    ║\n╟──────────┼───────────────────────╢\n║  POST    │  /testing             ║\n╟──────────┼───────────────────────╢\n║  DELETE  │  /long-url-path-name  ║\n╚══════════╧═══════════════════════╝')
   }) // end it
 
 }) // end UTILITY tests
