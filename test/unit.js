9bba041483ee3ed32d13a1818bd99d22a9862198
Jeremy Daly <jeremy@jeremydaly.com>
2018-08-14 12:49:08 -0400

    unit tests for route creation

---
+++ test/unit.js
@@ -0,0 +1,49 @@
+'use strict';
+
+const Promise = require('bluebird') // Promise library
+const expect = require('chai').expect // Assertion library
+
+// Init API instance
+const api = require('../index')({ version: 'v1.0' })
+
+
+// NOTE: Set test to true
+api._test = true;
+
+let event = {
+  httpMethod: 'get',
+  path: '/test',
+  body: {},
+  headers: {
+    'Content-Type': 'application/json'
+  }
+}
+
+/******************************************************************************/
+/***  BEGIN TESTS                                                           ***/
+/******************************************************************************/
+
+describe('Unit Tests:', function() {
+
+  it('setRoute - string path', async function() {
+    let routes = {}
+    api.setRoute(routes,{['_GET']: { route: '/testPath' } },['testPath'])
+    api.setRoute(routes,{['_GET']: { route: '/testPath/testx' } },'testPath.testx')
+    expect(routes).to.deep.equal({ testPath: { _GET: { route: '/testPath' }, testx: { _GET: { route: '/testPath/testx' } } } })
+  }) // end it
+
+
+  it('setRoute - null path', async function() {
+    let routes = { testPath: null }
+    api.setRoute(routes,{['_GET']: { route: '/testPath/testx' } },'testPath.testx')
+    expect(routes).to.deep.equal({ testPath: { testx: { _GET: { route: '/testPath/testx' } } } })
+  }) // end it
+
+  it('setRoute - null single path', async function() {
+    let routes = { testPath: null }
+    api.setRoute(routes,{['_GET']: { route: '/testPath' } },['testPath'])
+    expect(routes).to.deep.equal({ testPath: { _GET: { route: '/testPath' } } })
+  }) // end it
+
+
+}) // end UNIT tests
