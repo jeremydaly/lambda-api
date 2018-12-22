cdea6c9e8f0b4582d1520dbb8cb4869f14e57424
Jeremy Daly <jeremy@jeremydaly.com>
2018-12-22 11:33:57 -0500

    wip support for albs #80

--- test/requests.js
+++ test/requests.js
@@ -25,10 +25,13 @@ describe('Request Tests:', function() {
   describe('API Gateway Proxy Event', function() {
     it('Standard event', async function() {
       let _event = require('./sample-event-apigateway1.json')
-      let result = await new Promise(r => api.run(_event,{},(e,res) => { r(res) }))
+      let _context = require('./sample-context-apigateway1.json')
+      let result = await new Promise(r => api.run(_event,_context,(e,res) => { r(res) }))
       let body = JSON.parse(result.body)
+      // console.log(body);
       expect(result.headers).to.deep.equal({ 'content-type': 'application/json' })
       expect(body).to.have.property('request')
+      expect(body.request.id).is.not.null
       expect(body.request.userAgent).to.equal('Mozilla/5.0 (Macintosh; Intel Mac OS X 10_11_6) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/52.0.2743.82 Safari/537.36 OPR/39.0.2256.48')
       expect(body.request).to.have.property('requestContext')
       expect(body.request.ip).to.equal('192.168.100.1')
@@ -42,16 +45,17 @@ describe('Request Tests:', function() {
       expect(body.request.query.qs2).to.equal('bar')
       expect(body.request.multiValueQuery.qs2).to.deep.equal(['foo','bar'])
       expect(body.request.multiValueQuery.qs3).to.deep.equal(['bat','baz'])
-
     })
 
     it('Missing X-Forwarded-For (sourceIp fallback)', async function() {
       let _event = require('./sample-event-apigateway1.json')
+      let _context = require('./sample-context-apigateway1.json')
       delete _event.headers['X-Forwarded-For'] // remove the header
-      let result = await new Promise(r => api.run(_event,{},(e,res) => { r(res) }))
+      let result = await new Promise(r => api.run(_event,_context,(e,res) => { r(res) }))
       let body = JSON.parse(result.body)
       expect(result.headers).to.deep.equal({ 'content-type': 'application/json' })
       expect(body).to.have.property('request')
+      expect(body.request.id).is.not.null
       expect(body.request.userAgent).to.equal('Mozilla/5.0 (Macintosh; Intel Mac OS X 10_11_6) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/52.0.2743.82 Safari/537.36 OPR/39.0.2256.48')
       expect(body.request).to.have.property('requestContext')
       expect(body.request.ip).to.equal('192.168.100.12')
@@ -67,6 +71,32 @@ describe('Request Tests:', function() {
       expect(body.request.multiValueQuery.qs3).to.deep.equal(['bat','baz'])
       // console.log(body);
     })
+  })
+
+  describe('ALB Event', function() {
+    it('Standard event', async function() {
+      let _event = require('./sample-event-alb1.json')
+      let _context = require('./sample-context-alb1.json')
+      let result = await new Promise(r => api.run(_event,_context,(e,res) => { r(res) }))
+      let body = JSON.parse(result.body)
+      //console.log(body);
+      expect(result.headers).to.deep.equal({ 'content-type': 'application/json' })
+      expect(body).to.have.property('request')
+      expect(body.request.id).is.not.null
+      expect(body.request.userAgent).to.equal('Mozilla/5.0 (Macintosh; Intel Mac OS X 10_11_6) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/52.0.2743.82 Safari/537.36 OPR/39.0.2256.48')
+      expect(body.request).to.have.property('requestContext')
+      expect(body.request.ip).to.equal('192.168.100.1')
+      expect(body.request.isBase64Encoded).to.equal(true)
+      expect(body.request.clientType).to.equal('unknown')
+      expect(body.request.clientCountry).to.equal('unknown')
+      expect(body.request.route).to.equal('/test/hello')
+      expect(body.request.query.qs1).to.equal('foo')
+      // expect(body.request.query.qs2).to.equal('bar')
+      // expect(body.request.multiValueQuery.qs2).to.deep.equal(['foo','bar'])
+      // expect(body.request.multiValueQuery.qs3).to.deep.equal(['bat','baz'])
+
+    })
+
   })
 
 }) // end Request tests
