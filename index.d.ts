1607dfa6689c987bca810042e9cc64dc44ca7c50
Marco Klein <marco.klein@avsn.de>
2021-02-08 10:48:55 +0100

    Update index.d.ts

--- index.d.ts
+++ index.d.ts
@@ -99,6 +99,7 @@ export declare interface Options {
   mimeTypes?: {
     [key: string]: string;
   };
+  headerStyle: 'http1'|'http2';
   serializer?: SerializerFunction;
   version?: string;
   errorHeaderWhitelist?: string[];
