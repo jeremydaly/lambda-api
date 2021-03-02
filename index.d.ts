4dd392d01b9913ec3dc01367388d97f92e7aae2a
Joakim Hedlund <Sleavely@users.noreply.github.com>
2021-03-02 17:14:20 +0100

    Update LoggerFunction in accordance with README

--- index.d.ts
+++ index.d.ts
@@ -46,10 +46,7 @@ export declare type Middleware = (req: Request, res: Response, next: () => void)
 export declare type ErrorHandlingMiddleware = (error: Error, req: Request, res: Response, next: () => void) => void;
 export declare type ErrorCallback = (error?: Error) => void;
 export declare type HandlerFunction = (req: Request, res: Response, next?: NextFunction) => void | any | Promise<any>;
-declare function LogFn (message: string): void;
-declare function LogFn (obj: Record<string, any>): void;
-declare function LogFn (message: string, obj?: Record<string, any>): void
-export declare type LoggerFunction = typeof LogFn;
+export declare type LoggerFunction = (message: string, additionalInfo?: any) => void;
 export declare type NextFunction = () => void;
 export declare type TimestampFunction = () => string;
 export declare type SerializerFunction = (body: object) => string;
