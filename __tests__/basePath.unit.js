4da8a7693be1a08fad598211d80e1614a98c02f9
Naor Peled <thehecticbyte@gmail.com>
2023-02-03 16:45:55 +0200

    wip

--- __tests__/basePath.unit.js
+++ __tests__/basePath.unit.js
@@ -17,9 +17,21 @@ let event = {
   }
 }
 
+api.use(function (req, res, next) {
+  console.log('HELLO FROM MIDDLEWARE')
+  req.testMiddleware = "123";
+  next();
+});
+
 /******************************************************************************/
 /***  DEFINE TEST ROUTES                                                    ***/
 /******************************************************************************/
+api.get("/", function(req, res) {
+  res.status(200).json({
+    testMiddleware: req.testMiddleware,
+  })
+});
+
 api.get('/test', function(req,res) {
   res.status(200).json({ method: 'get', status: 'ok' })
 })
@@ -39,6 +51,12 @@ api.get('/test/test2/test3', function(req,res) {
 /******************************************************************************/
 
 describe('Base Path Tests:', function() {
+
+  it('should return testMiddleware: 123 when calling the root path', async function() {
+    let _event = Object.assign({},event,{ path: '/v1' })
+    let result = await new Promise(r => api.run(_event,{},(e,res) => { r(res) }))
+    expect(result).toEqual({ multiValueHeaders: { 'content-type': ['application/json'] }, statusCode: 200, body: '{"method":"get","status":"ok"}', isBase64Encoded: false })
+  })
 
   it('Simple path with base: /v1/test', async function() {
     let _event = Object.assign({},event,{ path: '/v1/test' })
