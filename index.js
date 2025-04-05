d50c1fbc53a962fed49e5ae5e98e40abf610ff34
Naor Peled <thehecticbyte@gmail.com>
2025-04-05 20:11:57 +0300

    prettier

--- index.js
+++ index.js
@@ -42,8 +42,8 @@ class API {
         : {};
     this._compression =
       props &&
-        (typeof props.compression === 'boolean' ||
-          Array.isArray(props.compression))
+      (typeof props.compression === 'boolean' ||
+        Array.isArray(props.compression))
         ? props.compression
         : false;
 
@@ -84,7 +84,7 @@ class API {
     this._app = {};
 
     // Executed after the callback
-    this._finally = () => { };
+    this._finally = () => {};
 
     // Global error status (used for response parsing errors)
     this._errorStatus = 500;
@@ -213,8 +213,8 @@ class API {
               stack: _stack['m'][method]
                 ? _stack['m'][method].concat(stack)
                 : _stack['*'][method]
-                  ? _stack['*'][method].concat(stack)
-                  : stack,
+                ? _stack['*'][method].concat(stack)
+                : stack,
               // inherited: _stack[method] ? _stack[method] : [],
               route: '/' + parsedPath.join('/'),
               path: '/' + this._prefix.concat(parsedPath).join('/'),
@@ -447,8 +447,8 @@ class API {
       typeof args[0] === 'string'
         ? Array.of(args.shift())
         : Array.isArray(args[0])
-          ? args.shift()
-          : ['/*'];
+        ? args.shift()
+        : ['/*'];
 
     // Init middleware stack
     let middleware = [];
